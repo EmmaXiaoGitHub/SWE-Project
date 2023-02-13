7bb13b619076371ecd7ace4ca9670e845a2880d0
EmmaXiaoGitHub <125226471+EmmaXiaoGitHub@users.noreply.github.com>
2023-02-13 14:44:30 -0800

    TODO: Perform logout action here, removing the username from local storage     // Redirect the user to the login page

--- homework1/ui/src/components/LogoutButton.jsx
+++ homework1/ui/src/components/LogoutButton.jsx
@@ -4,6 +4,8 @@ const LogoutButton = () => {
   const handleClick = () => {
     // TODO: Perform logout action here, removing the username from local storage
     // Redirect the user to the login page
+    localStorage.removeItem('username');
+    window.location.href = '/login';
   };
 
   return (
